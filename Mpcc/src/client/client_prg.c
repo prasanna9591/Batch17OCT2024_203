053b28099e4c9be3c2302bee82f3f66137d389ca
Aman Sharma <sharma.aman.sa456@gmail.com>
2024-12-16 09:16:11 +0000

    updated

--- Mpcc/src/client/client_prg.c
+++ Mpcc/src/client/client_prg.c
@@ -3,35 +3,104 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include "client_send_recv.h"
-#include "log_client_report.h"
+#include <pthread.h>
+#include <arpa/inet.h>
+
+
 
 #define SERVER_ADDR "127.0.0.1"
 #define PORT 8080
 
+int sock;  // Global socket for sending and receiving messages
+
+// Function for receiving messages from the server
+void *receive_messages(void *arg) {
+    char buffer[1024];
+    int bytes_received;
+    
+    while (1) {
+        bytes_received = recv(sock, buffer, sizeof(buffer) - 1, 0);
+        if (bytes_received <= 0) {
+            if (bytes_received == 0) {
+                printf("Server disconnected\n");
+            } else {
+                perror("recv failed");
+            }
+            break;
+        }
+
+        buffer[bytes_received] = '\0';  // Null-terminate the received message
+        printf("Received: %s\n", buffer);  // Print received message
+    }
+    
+    return NULL;
+}
+
+// Function for sending messages to the server
+void *send_messages(void *arg) {
+    char message[1024];
+
+    while (1) {
+        // Get message from user
+        printf("Enter message: ");
+        fgets(message, sizeof(message), stdin);
+        message[strcspn(message, "\n")] = 0; // Remove newline character
+        
+        if (send(sock, message, strlen(message), 0) == -1) {
+            perror("send failed");
+            break;
+        }
+    }
+
+    return NULL;
+}
+
 int main() {
-    int sock;
     struct sockaddr_in server_addr;
 
+    // Create the socket
     sock = socket(AF_INET, SOCK_STREAM, 0);
     if (sock == -1) {
         perror("Socket creation failed");
         exit(EXIT_FAILURE);
     }
 
+    // Set up the server address
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(PORT);
     server_addr.sin_addr.s_addr = inet_addr(SERVER_ADDR);
 
+    // Connect to the server
     if (connect(sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
         perror("Connection failed");
         close(sock);
         exit(EXIT_FAILURE);
     }
 
-    client_send_recv(sock);
+    printf("Connected to the server at %s:%d\n", SERVER_ADDR, PORT);
 
+    // Create threads for sending and receiving messages
+    pthread_t recv_thread, send_thread;
+
+    // Create a thread for receiving messages
+    if (pthread_create(&recv_thread, NULL, receive_messages, NULL) != 0) {
+        perror("Thread creation failed for receiving messages");
+        close(sock);
+        exit(EXIT_FAILURE);
+    }
+
+    // Create a thread for sending messages
+    if (pthread_create(&send_thread, NULL, send_messages, NULL) != 0) {
+        perror("Thread creation failed for sending messages");
+        close(sock);
+        exit(EXIT_FAILURE);
+    }
+
+    // Wait for both threads to finish
+    pthread_join(recv_thread, NULL);
+    pthread_join(send_thread, NULL);
+
+    // Close the socket when done
     close(sock);
     return 0;
 }
-
--- Mpcc/src/server/server_prg.c
+++ Mpcc/src/server/server_prg.c
@@ -1,40 +1,95 @@
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <unistd.h>
+// server_prg.c
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <pthread.h>  // Ensure pthread.h is included for thread functions
-#include "client_handler.h"   // Make sure this file contains the correct declaration of handle_client
-#include "log_server_report.h" // Your logging functionality
+#include <unistd.h>
+#include <pthread.h>
+#include <arpa/inet.h>
 
 #define PORT 8080
 #define MAX_CLIENTS 10
 
-// Declare the mutex to synchronize client access
-pthread_mutex_t clients_mutex = _PTHREAD_MUTEX_INIIALIZER;
-int clients[MAX_CLIENTS]; // To hold active client sockets
+// Global array to store client sockets
+int clients[MAX_CLIENTS] = {0}; 
 
-// Handle client function prototype (ensure this function is defined correctly in client_handler.c)
-void *handle_client(void *arg);  // Function prototype for handle_client
+// Mutex for client array
+pthread_mutex_t clients_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+// Function to handle communication with each client
+void *handle_client(void *arg) {
+    int client_sock = *((int *)arg);  // Get the client socket
+    free(arg);  // Free the allocated memory
+    char buffer[1024];
+    int bytes_received;
+
+    // Add client to the global client array
+    pthread_mutex_lock(&clients_mutex);
+    for (int i = 0; i < MAX_CLIENTS; i++) {
+        if (clients[i] == 0) {
+            clients[i] = client_sock;
+            break;
+        }
+    }
+    pthread_mutex_unlock(&clients_mutex);
+
+    printf("New client connected: %d\n", client_sock);
+
+    // Listen for messages from the client
+    while (1) {
+        bytes_received = recv(client_sock, buffer, sizeof(buffer) - 1, 0);
+        if (bytes_received <= 0) {
+            if (bytes_received == 0) {
+                printf("Client %d disconnected\n", client_sock);
+            } else {
+                perror("recv failed");
+            }
+            break;
+        }
+
+        buffer[bytes_received] = '\0';  // Null-terminate the received message
+        printf("Received from client %d: %s\n", client_sock, buffer);
+
+        // Broadcast the message to all other clients
+        pthread_mutex_lock(&clients_mutex);
+        for (int i = 0; i < MAX_CLIENTS; i++) {
+            if (clients[i] != 0 && clients[i] != client_sock) {
+                send(clients[i], buffer, bytes_received, 0);
+            }
+        }
+        pthread_mutex_unlock(&clients_mutex);
+    }
+
+    // Remove client from the global client array
+    pthread_mutex_lock(&clients_mutex);
+    for (int i = 0; i < MAX_CLIENTS; i++) {
+        if (clients[i] == client_sock) {
+            clients[i] = 0;
+            break;
+        }
+    }
+    pthread_mutex_unlock(&clients_mutex);
+
+    // Close the client socket
+    close(client_sock);
+    return NULL;
+}
 
 int main() {
     int server_fd, new_socket;
     struct sockaddr_in server_addr, client_addr;
     socklen_t client_len = sizeof(client_addr);
 
-    // Create the server socket
+    // Create server socket
     server_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (server_fd == -1) {
         perror("Server socket creation failed");
         exit(EXIT_FAILURE);
     }
 
-    // Prepare the server address
+    // Set up the server address
     server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = INADDR_ANY;
     server_addr.sin_port = htons(PORT);
+    server_addr.sin_addr.s_addr = INADDR_ANY;
 
     // Bind the socket to the address
     if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
@@ -60,18 +115,19 @@ int main() {
             continue;
         }
 
+        // Create a new thread for the client
         pthread_t tid;
-        int *new_sock = malloc(sizeof(int));
-        *new_sock = new_socket;
+        int *client_sock = malloc(sizeof(int));
+        *client_sock = new_socket;
 
-        // Create a thread to handle each client
-        if (pthread_create(&tid, NULL, handle_client, (void *)new_sock) != 0) {
+        // Create a thread to handle the client
+        if (pthread_create(&tid, NULL, handle_client, (void *)client_sock) != 0) {
             perror("Thread creation failed");
-            free(new_sock);
+            free(client_sock);
         }
     }
 
-    close(server_fd); // Close the server socket when done
+    close(server_fd);  // Close the server socket when done
     return 0;
 }
 
